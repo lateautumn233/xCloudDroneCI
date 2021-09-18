d1f74e36dad36c567f53567c35c312be4bc80ad7
lateautumn233 <3487467850@qq.com>
2021-09-18 20:00:40 +0800

    set late tc-llvm

--- llvm-tc_build.sh
+++ llvm-tc_build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # Use xRageTC build script as LLVM Build Script.
-git clone https://github.com/xyz-prjkt/llvmTC $(pwd)/llvmTC -b llvm-tc
+git clone https://github.com/lateautumn233/llvmTC $(pwd)/llvmTC -b llvm-tc
 cd $(pwd)/llvmTC
 bash build-tc.sh
